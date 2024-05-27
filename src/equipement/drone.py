548ac8e3714ac8d9edb64839bc8debdd3f53c90a
terence miralves <terence.miralves@gmail.com>
2024-05-27 14:45:45 +0200

    added the classes of drone and snowplows

--- src/equipement/drone.py
+++ src/equipement/drone.py
@@ -1,3 +1,11 @@
 """
 ! This class will hold the class for drones, it will have the price information and the drone type
 """
+
+class Drone:
+    """
+    ! This class will hold the class for drones, it will have the price information
+    """
+    def __init__(self):
+        self.fixed_cost = 100
+        self.Cost_km = 0.01
--- src/equipement/snowplow.py
+++ src/equipement/snowplow.py
@@ -1,3 +1,27 @@
 """
 ! This file contatins the class of snowplows, it has the price information and the snowplow type
 """
+
+class Snowplow:
+    """
+    ! This class will hold the price info and the type of snowplow
+    """
+    def __init__(self, type : int):
+        """
+        ! This function will initialize the class
+        @param type: int: the type of snowplow, can be 1 or 2
+        """
+        if type == 1:
+            self.fixed_cost = 500
+            self.cost_km = 1.1
+            self.cost_first_8_hours = 1.1
+            self.cost_after_8_hours = 1.3
+            self.speed = 10
+        elif type == 2:
+            self.fixed_cost = 800
+            self.cost_km = 1.3
+            self.cost_first_8_hours = 1.3
+            self.cost_after_8_hours = 1.5
+            self.speed = 20
+        else:
+            raise ValueError("Usage error: snowplow type must be 1 or 2")
