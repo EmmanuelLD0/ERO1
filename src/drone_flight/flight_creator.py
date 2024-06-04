bffda538039e655a4af7f7ae9b54966512c155fe
David Calderon <david-ortiz.calderon@epita.fr>
2024-06-04 18:48:44 +0200

    fix: readded euler

--- src/drone_flight/flight_creator.py
+++ src/drone_flight/flight_creator.py
@@ -8,6 +8,44 @@ from tools.dijikstra import dijikstra
 from itertools import combinations
 from datetime import datetime, timedelta
 
+def is_eulerian(G : nx.Graph):
+    """
+    ! This function will check if the graph is eulerian
+    @param G: nx.Graph: the graph of the city
+    @return: bool: if the graph is eulerian
+    """
+    for node in G.nodes:
+        if G.degree[node] % 2 != 0:
+            return False
+    return True
+
+def find_eulerian_path_rec(G : nx.Graph, actual : int):
+    """
+    ! This function will find the eulerian path of the graph recursively
+    @param G: nx.Graph: the graph of the city
+    @param actual: int: the actual node
+    @return: list: the eulerian path of the graph
+    """
+    if (len(G.edges) == 0):
+        return []
+    for edge in G.edges(actual):
+        next_node = edge[1]
+        G.remove_edge(actual, next_node)
+        path = find_eulerian_path_rec(G, next_node)
+        if path is None:
+            G.add_edge(actual, next_node)
+            continue
+        return [actual] + path
+    return None
+
+def find_eulerian_circuit(G : nx.Graph):
+    """
+    ! This function will find the eulerian circuit of the graph
+    @param G: nx.Graph: the graph of the city
+    @return: list: the eulerian circuit of the graph
+    """
+    return find_eulerian_path_rec(G, list(G.nodes)[0])
+
 def cpp(G : nx.Graph):
     """
     ! This function will create the flight pattern of the drones
@@ -77,3 +115,6 @@ def create_flight_pattern(G : nx.Graph):
     #Calculate the price
     price = calculate_price(G, flight_path, drone)
     return flight_path, price
+
+
+
