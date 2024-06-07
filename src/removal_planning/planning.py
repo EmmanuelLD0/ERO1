77e595187ac228372a518b4490278c4efcb8dbbe
Maxime Bardouil <maxime.bardouil10@gmail.com>
2024-06-07 06:14:07 +0200

    working snowplows

--- src/removal_planning/planning.py
+++ src/removal_planning/planning.py
@@ -1,8 +1,124 @@
 """
-! This file will create the removal planning for the following sectors:
-! - Outremont,
-! - Verdun,
-! - Anjou,
-! - Rivière-des-Prairies,
-! - Le plateau-Mont-Royal,
+! This file will create the flight pattern of the drones
 """
+
+import osmnx as ox
+import networkx as nx
+from equipement.drone import Drone
+from tools.dijikstra import dijikstra
+from itertools import combinations
+from datetime import datetime, timedelta
+from copy import copy
+
+
+def truck_paths(G, n_trucks):
+    temp = get_path(G)
+    paths = []
+    for i in range(len(temp)):
+        path = []
+        for j in range(len(temp[i]) - 1):
+            path.append((temp[i][j], temp[i][j + 1]))
+        paths.append(path)
+    while len(paths) < n_trucks:
+        m = -1
+        v = -1
+        for i in range(len(paths)):
+            if len(paths[i]) > v:
+                m = i
+                v = len(paths[i])
+        if m == -1:
+            break
+        t1 = paths[m][:len(paths[m])//2]
+        t2 = paths[m][len(paths[m])//2:]
+        paths[m] = t2
+        paths.append(t1)
+    return paths
+
+
+def get_path(G):
+    if nx.is_empty(G):
+        return []
+    dE = G.edges
+    if dE == []:
+        return []
+    unG = nx.Graph()
+    edges_left = nx.DiGraph()
+    for edge in dE:
+        unG.add_edge(edge[0],edge[1])
+        edges_left.add_edge(edge[0],edge[1])
+    print("ung:")
+    print(unG.edges)
+    try:
+        unG = nx.eulerize(unG)
+    except Exception as e:
+        res = []
+        components = [G.subgraph(c).copy() for c in nx.strongly_connected_components(G)]
+        for g in components:
+            print("subgraph len")
+            print(len(g.edges))
+            res += (get_path(g))
+        return res
+    path = []
+    path = list(nx.eulerian_path(unG))
+    i = len(path) - 1
+    while i >= 0:
+        start = path[:-1]
+        (a,b) = path[i]
+        if (a,b) in start or (b,a) in start:
+            path.pop(len(path) - 1)
+        else:
+            break
+        i -= 1
+    reg = 0
+    rev = 0
+    for (a,b) in path:
+        if (a,b) not in dE:
+            reg += 1
+        if (b,a) not in dE:
+            rev += 1
+    if reg > rev:
+        path.reverse()
+        for i in range(len(path)):
+            (a,b) = path[i]
+            path[i] = (b,a)
+    le = len(path) - 1
+    res = []
+    for (a, b) in path:
+        res.append(a)
+        path_ex = False
+        if (a, b) in dE:
+            try:
+                edges_left.remove_edge(a,b)
+            except:
+                pass
+            try:
+                edges_left.remove_edge(b,a)
+            except:
+                pass
+            continue
+        try:
+            sP = nx.shortest_path(G, a, b)
+        except:
+            print((a,b))
+            print(res)
+            return [res] + get_path(edges_left)
+        for i in range(len(sP) - 1):
+            try:
+                edges_left.remove_edge(sP[i], sP[i + 1])
+            except:
+                pass
+        res += sP[1:-1]
+        if (b, a) in dE and (b,a) not in path:
+            try:
+                edges_left.remove_edge(a,b)
+            except:
+                pass
+            try:
+                edges_left.remove_edge(b,a)
+            except:
+                pass
+            res.append(b)
+            res += sP[:-1]
+    res.append(path[le][1])
+    print(res)
+    return [res]
